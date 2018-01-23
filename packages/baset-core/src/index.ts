fbf5eb0d7e3960634ff5a8f5313fea66cceb532f
Igmat (Travis CI) <ichulinda@gmail.com>
2018-01-23 19:21:32 +0100

    feat(Core): Recursivly check exports to work with promises and functions

--- packages/baset-core/src/index.ts
+++ packages/baset-core/src/index.ts
@@ -1,14 +1,27 @@
 import * as fs from 'fs';
 import * as beautify from 'json-beautify';
 import * as path from 'path';
+import { isPrimitive } from 'util';
 
-export function test(specs: string[], baselines: string[]): Promise<string>[] {
-    const files = specs.map(name => ({
-        name,
-        output: beautify(require(path.resolve(name)), undefined, 4, 20),
-    }));
+// tslint:disable-next-line:no-any
+async function calculateValues(obj: any): Promise<any> {
+    if (isPrimitive(obj)) return obj;
+    else if (obj instanceof Promise) return obj;
+    else if (obj instanceof Function) return obj();
+    else if (obj instanceof Array) return await Promise.all(obj.map(calculateValues));
 
-    return files.map(file => new Promise((resolve, reject) => {
+    return (await Promise.all(Object.keys(obj)
+        .map(async key => ({ [key]: await calculateValues(obj[key]) }))))
+        .reduce((result, prop) => ({ ...result, ...prop }), {});
+}
+
+async function testSpec(spec: string) {
+    const file = {
+        name: spec,
+        output: beautify(await calculateValues(require(path.resolve(spec))), undefined, 4, 20),
+    };
+
+    return await new Promise<string>((resolve, reject) => {
         const baselinePath = path.resolve(file.name.replace(/.spec.js$/, '.base'));
         const baseline = fs.existsSync(baselinePath)
             ? fs.readFileSync(baselinePath, { encoding: 'utf-8' })
@@ -17,11 +30,7 @@ export function test(specs: string[], baselines: string[]): Promise<string>[] {
             path.resolve(file.name.replace(/.spec.js$/, '.base.tmp')),
             file.output,
             err => {
-                if (err) {
-                    console.log(err);
-
-                    return reject(err);
-                }
+                if (err) return reject(err);
                 console.log(`Temp baseline for ${file.name} is written.`);
                 if (baseline === file.output) {
                     resolve(`Test for ${file.name} is passed`);
@@ -33,7 +42,11 @@ export function test(specs: string[], baselines: string[]): Promise<string>[] {
                     });
                 }
             });
-    }));
+    });
+}
+
+export function test(specs: string[], baselines: string[]): Promise<string>[] {
+    return specs.map(testSpec);
 }
 
 export function accept(files: string[]) {
--- tests/sample-project/index.spec.js
+++ tests/sample-project/index.spec.js
@@ -7,5 +7,7 @@ module.exports = {
         sampleFn('abc', 'cba'),
         sampleFn(1, 'abc'),
         sampleFn('abc', 1),
+        () => sampleFn('function call', 1),
+        new Promise(resolve => resolve(sampleFn('async value', 1)))
     ]
 }
