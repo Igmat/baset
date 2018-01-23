3037ab2c13153c6fdefcc08d8bb6f6fe78d1f132
Igor Chulinda <igor.chulinda@dataart.com>
2018-01-23 14:40:37 +0100

    ci(Solution): Update yarn.lock, auth script and .npmignore for avoiding problems with releases
    fix #1

--- scripts/authenticate.sh
+++ scripts/authenticate.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 setup_git() {
-    git config user.email "ichulinda@gmail.com"
-    git config user.name "Igmat (Travis CI)"
+    git config --global user.email "ichulinda@gmail.com"
+    git config --global user.name "Igmat (Travis CI)"
     git remote remove origin
     git remote add origin https://${GH_TOKEN}@github.com/Igmat/baset.git
+    git checkout master
 }
 
 setup_npm() {
