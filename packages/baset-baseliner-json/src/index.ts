7ea8f007c931fb2484be8055e0c9f57cf04b6474
Ihor Chulinda <ichulinda@dataart.com>
2018-02-26 20:05:38 +0100

    fix(baseliner-json): reflect circular dependencies in baseline
    re #43

--- packages/baset-baseliner-json/src/index.ts
+++ packages/baset-baseliner-json/src/index.ts
@@ -1,12 +1,13 @@
-import { AbstractBaseliner } from 'baset-core';
-import beautify from 'json-beautify';
+import { AbstractBaseliner, circularReference } from 'baset-core';
+import { beautify } from './beautifier';
 
 export default class ExportReader extends AbstractBaseliner {
     create = async (result: Promise<any>[]) => {
         const results = await Promise.all(result);
+        const isExportSingle = results.length === 1;
 
-        return (results.length > 1)
-            ? beautify(results, undefined, 4, 20)
-            : beautify(results[0], undefined, 4, 20);
+        return (!isExportSingle)
+            ? beautify(results, undefined, 4, 20, isExportSingle)
+            : beautify(results[0], undefined, 4, 20, isExportSingle);
     }
 }
--- packages/baset-baseliner-json/src/types/json-beautify.d.ts
+++
@@ -1,5 +0,0 @@
-declare interface beautify {
-    (value: any, replacer?: (key: string, value: any) => any, space?: string | number, maxWidth?: number): string;
-}
-declare const beautify: beautify;
-export = beautify;
