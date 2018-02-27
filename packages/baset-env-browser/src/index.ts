a79c38deb0763924d6ae0866a4615d91a5705ab1
Ihor Chulinda <ichulinda@dataart.com>
2018-02-27 14:27:40 +0100

    fix(env-browser): setting jsdom window as prototype of global
    fix #42

--- packages/baset-env-browser/src/index.ts
+++ packages/baset-env-browser/src/index.ts
@@ -2,7 +2,7 @@ import { AbstractEnvironmet } from 'baset-core';
 import { JSDOM } from 'jsdom';
 
 const dom = new JSDOM();
-Object.assign(global, dom.window);
+Object.setPrototypeOf(global, dom.window);
 
 export default class BrowserEnv extends AbstractEnvironmet {
 }
