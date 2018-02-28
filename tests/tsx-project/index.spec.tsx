bd465a2a82a7049be2c50ce0fcd2eb3641a2183d
Ihor Chulinda <ichulinda@dataart.com>
2018-02-28 18:46:56 +0100

    test(resolver-react): e2e tests update with react resolver usage

--- tests/tsx-project/index.spec.tsx
+++ tests/tsx-project/index.spec.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom/server';
 import { jsxFn } from './index';
 
-export const value = ReactDOM.renderToString(
+export const value = (
     <div>
         {jsxFn('s', 's')}
         {jsxFn('abc', 'cba')}
         {jsxFn('s', 'abc')}
         {jsxFn('abc', 's')}
-    </div>,
+    </div>
 );
