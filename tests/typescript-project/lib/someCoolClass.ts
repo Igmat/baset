e7d65167147028d8465953de23bf00a8726084aa
Ihor Chulinda <ichulinda@dataart.com>
2018-02-28 12:50:44 +0100

    test(solution): e2e test for global imports

--- tests/typescript-project/lib/someCoolClass.ts
+++ tests/typescript-project/lib/someCoolClass.ts
@@ -1,7 +1,13 @@
+function logParamTypes(target: any, key: string) {
+    const types = Reflect.getMetadata('design:paramtypes', target, key);
+    const s = types.map((a: any) => a.name).join();
+}
+
 export class SomeCoolClass {
     private a = 1000;
     private b = 300;
 
+    @logParamTypes
     getSum() {
         return this.a + this.b;
     }
