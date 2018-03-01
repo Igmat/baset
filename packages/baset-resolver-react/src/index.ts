36369ebab77eb2af22f11e2af362ff3d79ec64c7
Ihor Chulinda <ichulinda@dataart.com>
2018-03-01 18:22:25 +0100

    fix(resolver-react): using html data type for beautified baseline

--- packages/baset-resolver-react/src/index.ts
+++ packages/baset-resolver-react/src/index.ts
@@ -1,10 +1,12 @@
-import { AbstractResolver } from 'baset-core';
+import { AbstractResolver, dataTypes } from 'baset-core';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 
 export default class ReactResolver extends AbstractResolver {
     match = async (obj: any) =>
         React.isValidElement(obj);
-    resolve = async (obj: any) =>
-        ReactDOM.renderToString(obj);
+    resolve = async (obj: any) => ({
+        value: ReactDOM.renderToString(obj),
+        [dataTypes.html]: 'react',
+    });
 }
