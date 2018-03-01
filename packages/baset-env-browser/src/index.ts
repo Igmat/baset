c354f44456c0d9818655d53ab8046a2f7d55cb2c
Ihor Chulinda <ichulinda@dataart.com>
2018-03-01 12:59:00 +0100

    fix(env-browser): better support for external resources

--- packages/baset-env-browser/src/index.ts
+++ packages/baset-env-browser/src/index.ts
@@ -1,7 +1,12 @@
 import { AbstractEnvironmet } from 'baset-core';
-import { JSDOM } from 'jsdom';
+import { ConstructorOptions, JSDOM } from 'jsdom';
 
-const dom = new JSDOM();
+// FIXME: seems like jsdom.d.ts is incorrect and `pretendToBeVisual` is missing
+// tslint:disable-next-line:no-object-literal-type-assertion
+const dom = new JSDOM('', {
+    resources: 'usable',
+    pretendToBeVisual: true,
+} as ConstructorOptions);
 Object.setPrototypeOf(global, dom.window);
 
 export default class BrowserEnv extends AbstractEnvironmet {
