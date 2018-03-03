832f3c9520088cec91a313cb0ebeabedfb593d90
Ihor Chulinda <ichulinda@dataart.com>
2018-03-03 14:46:49 +0100

    feat(baseliner-md): add comparison for json, html and images in md

--- packages/baset-baseliner-md/src/index.ts
+++ packages/baset-baseliner-md/src/index.ts
@@ -1,7 +1,11 @@
 import JSONBaseliner from 'baset-baseliner-json';
-import { AbstractBaseliner, circularReference, dataTypes } from 'baset-core';
+import { AbstractBaseliner, circularReference, dataTypes, utils } from 'baset-core';
 import htmlBeautify from 'html-beautify';
+import { markdown } from 'markdown';
+import pixelmatch from 'pixelmatch';
+import { PNG } from 'pngjs';
 import { isPrimitive } from 'util';
+
 interface IKnownType {
     type: typeof dataTypes.image | typeof dataTypes.html;
     name: string;
@@ -17,7 +21,7 @@ const knownTypeTemplates = {
 `,
     [dataTypes.html]: (html: IKnownType) => `
 \`\`\`HTML
-${htmlBeautify(html.src)}
+${html.src}
 \`\`\`
 `,
 };
@@ -52,6 +56,55 @@ export default class MDBaseliner extends AbstractBaseliner {
 
         return mdTemplate(await this.jsonBaseliner.create(results), known);
     }
+    compare = async (result: Promise<any>[], baseline: Promise<string>) => {
+        const [results, baselineValue] = await Promise.all([Promise.all(result), baseline]);
+        const known = results.length === 1
+            ? this.cutKnownTypes(results[0], 'exports')
+            : this.cutKnownTypes(results, 'exports');
+        const oldBase = this.parse(baselineValue);
+        const newJsonValues = await this.jsonBaseliner.create(results);
+        let isEqual = true;
+        if (utils.normalizeEndings(newJsonValues) !== utils.normalizeEndings(oldBase.json)) isEqual = false;
+        if (!await compareKnownTypes(oldBase.knownEntities, known)) isEqual = false;
+
+        return {
+            isEqual,
+            expected: baselineValue,
+            actual: utils.normalizeEndings(mdTemplate(newJsonValues, known)),
+            diff: {
+                console: '',
+                full: '',
+            },
+        };
+    }
+    private parse = (baseline: string) => {
+        const mdTree = markdown.parse(baseline);
+        const paraNodes: any[] = mdTree.slice(1);
+        const json: string = (paraNodes[0][1] === 'JSON values:\n')
+            ? paraNodes[0][2][1].replace(/^JSON\n/, '')
+            : '{}';
+        const firstKnownTypeIndex = Number(json !== '{}');
+        const knownEntities: IKnownType[] = [];
+        for (let i = firstKnownTypeIndex; i < paraNodes.length; i += 2) {
+            const type = paraNodes[i + 1][1][0] === 'img'
+                ? dataTypes.image
+                : dataTypes.html;
+            const name = paraNodes[i][1][1].replace(':', '');
+            const src = (type === dataTypes.image)
+                ? paraNodes[i + 1][1][1].href
+                : paraNodes[i + 1][1][1].replace(/^HTML\n/, '');
+            knownEntities.push({
+                name,
+                type,
+                src,
+            });
+        }
+
+        return {
+            json,
+            knownEntities,
+        };
+    }
 
     private cutKnownTypes = (parent: any, path: string, key?: string | number): IKnownType[] => {
         const obj = (key !== undefined)
@@ -72,7 +125,9 @@ export default class MDBaseliner extends AbstractBaseliner {
             const result: IKnownType = {
                 type,
                 name: path,
-                src: obj.value,
+                src: (type === dataTypes.html)
+                    ? htmlBeautify(obj.value)
+                    : obj.value,
             };
             if (key !== undefined) delete parent[key];
 
@@ -82,4 +137,53 @@ export default class MDBaseliner extends AbstractBaseliner {
         return ([] as IKnownType[]).concat(...Object.keys(obj)
             .map(index => this.cutKnownTypes(obj, `${path}.${index}`, index)));
     }
+}
+
+async function compareKnownTypes(oldBase: IKnownType[], newBase: IKnownType[]) {
+    const sortedOldBase = oldBase.sort(sortKnownTypes);
+    const sortedNewBase = newBase.sort(sortKnownTypes);
+    for (let i = 0; i < sortedOldBase.length; i++) {
+        const value = sortedOldBase[i];
+        const newValue = sortedNewBase[i];
+        if (newValue === undefined) return false;
+        if (value.type !== newValue.type) return false;
+        if (value.type === dataTypes.image) {
+            if (!await compareImages(value.src, newValue.src)) return false;
+        } else {
+            if (utils.normalizeEndings(value.src) !== utils.normalizeEndings(newValue.src)) return false;
+        }
+    }
+
+    return true;
+}
+
+async function compareImages(a: string, b: string) {
+    // base64 strings are platform dependent and differs on win and *nix
+    // so we'll have to match them pixel by pixel
+    const [imageA, imageB] = await Promise.all([base64ToPNG(a), base64ToPNG(b)]);
+    if (imageA.width !== imageB.width || imageA.height !== imageB.height) return false;
+
+    return pixelmatch(imageA.data, imageB.data, null, imageA.width, imageA.height, {
+        includeAA: true,
+        threshold: 0,
+    }) === 0;
+}
+
+const base64prefix = 'data:image/png;base64,';
+async function base64ToPNG(base64String: string) {
+    return new Promise<PNG>(async (resolve, reject) => {
+        const imgBuffer = new Buffer(base64String.replace(base64prefix, ''), 'base64');
+        const png = new PNG();
+        png.parse(imgBuffer, err => (!err)
+            ? resolve(png)
+            : reject(err));
+    });
+}
+
+function sortKnownTypes(a: IKnownType, b: IKnownType) {
+    return a.name > b.name
+        ? 1
+        : a.name < b.name
+            ? -1
+            : 0;
 }
