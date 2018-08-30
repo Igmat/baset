1703b439c918761fda107a47134f799ee22d6860
Ihor Chulinda <ichulinda@gmail.com>
2018-08-30 18:05:34 +0200

    feat(resolver-react): using react-test-render instead of react-dom

--- packages/baset-resolver-react/src/index.ts
+++ packages/baset-resolver-react/src/index.ts
@@ -1,12 +1,17 @@
 import { AbstractResolver, dataTypes } from 'baset-core';
-import React from 'react';
-import ReactDOM from 'react-dom/server';
+import prettyFormat from 'pretty-format';
+import React, { ReactElement } from 'react';
+import ReactTestRenderer from 'react-test-renderer';
+const { ReactElement, ReactTestComponent } = prettyFormat.plugins;
 
 export default class ReactResolver extends AbstractResolver {
-    match = async (obj: any) =>
+    match = async (obj: {} | null | undefined) =>
         React.isValidElement(obj);
-    resolve = async (obj: any) => ({
-        value: ReactDOM.renderToString(obj),
+    resolve = async (obj: ReactElement<unknown>) => ({
+        value: prettyFormat(ReactTestRenderer.create(obj).toJSON(), {
+            plugins: [ReactTestComponent],
+            printFunctionName: false,
+        }),
         [dataTypes.html]: 'react',
     });
 }
