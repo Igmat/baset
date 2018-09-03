477d2893e6e807ece0ccf9a4820ba2f25f1ac812
Ihor Chulinda <ichulinda@gmail.com>
2018-09-03 20:19:40 +0200

    test(solution): e2e test for usage with styled components lib

---
+++ tests/tsx-styled-components-project/src/App.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+import logo from './logo.svg';
+
+const Introduction = styled.p`
+    font-weight: 900;
+    font-size: large;
+`;
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Introduction>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </Introduction>
+      </div>
+    );
+  }
+}
+
+export default App;
