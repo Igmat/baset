b1814175879c2165030067eccbfdde52fece5cc4
Ihor Chulinda <ichulinda@gmail.com>
2018-10-10 16:00:23 +0200

    fix(baseliner-json): handle exceptions data type

--- packages/baset-baseliner-json/src/beautifier.ts
+++ packages/baset-baseliner-json/src/beautifier.ts
@@ -1,4 +1,4 @@
-import { circularReference } from 'baset-core';
+import { circularReference, dataTypes } from 'baset-core';
 
 const rxEscapable =
     /[\\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
@@ -70,6 +70,9 @@ function str(key: string | number, holder: any, limit: number): string {
                 return (shouldReplaceZeroIndex)
                     ? value[circularReference].replace(/^exports\[0\]/, 'exports')
                     : value[circularReference];
+            }
+            if (value && value[dataTypes.error]) {
+                return `Throws: ${value[dataTypes.error].stack || value[dataTypes.error]}`;
             }
 
             // Make an array to hold the partial results of stringifying this object value.
