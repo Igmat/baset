90fdc08b4e0accb98ced4ba9e4cc6da8038bdc93
Ihor Chulinda <ichulinda@gmail.com>
2019-06-10 00:05:38 +0200

    fix(env-browser): update jsdom and canvas deps

--- packages/baset-env-browser/src/runInContext.ts
+++ packages/baset-env-browser/src/runInContext.ts
@@ -1,11 +1,10 @@
-import { ConstructorOptions, JSDOM } from 'jsdom';
+import { JSDOM } from 'jsdom';
 declare const basetSandbox: { basetBrowserEnv__StaticUrl: string };
 
-// FIXME: seems like jsdom.d.ts is incorrect and `pretendToBeVisual` is missing
-// tslint:disable-next-line:no-object-literal-type-assertion
 const dom = new JSDOM('', {
     url: basetSandbox.basetBrowserEnv__StaticUrl,
     resources: 'usable',
     pretendToBeVisual: true,
-} as ConstructorOptions);
+
+});
 Object.setPrototypeOf(global, dom.window);
